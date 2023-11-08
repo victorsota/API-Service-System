5e82f297089839dff8c78e0079e3fd1f8c57061e
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-11-08 20:51:22 -0300

    Add CRUD Produto

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -26,4 +26,65 @@ export class ProdutoController {
       return res.status(500).json({ message: "Erro interno" });
     }
   }
+
+  async listar(req: Request, res: Response) {
+    try {
+      const produtos = await produtoRepository.find();
+      return res.status(200).json(produtos);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
+
+  async listarPorId(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const produto = await produtoRepository.findOne({ where: { id } });
+      return res.status(200).json(produto);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const produto = await produtoRepository.findOne({ where: { id } });
+      if (!produto) {
+        return res.status(404).json({ message: "Produto não encontrado" });
+      }
+      await produtoRepository.remove(produto);
+      return res.status(200).json({ message: "Produto removido com sucesso" });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { nome, descricao, preco, quantidade, tipoId } = req.body;
+
+    try {
+      const produto = await produtoRepository.findOne({ where: { id } });
+      if (!produto) {
+        return res.status(404).json({ message: "Produto não encontrado" });
+      }
+      const tipo = await TipoProdutoRepository.findOneBy({
+        id: Number(tipoId),
+      });
+      if (!tipo) {
+        return res.status(400).json({ message: "Tipo de produto não existe" });
+      }
+      produto.nome = nome;
+      produto.descricao = descricao;
+      produto.preco = preco;
+      produto.quantidade = quantidade;
+      produto.tipo = tipo;
+      await produtoRepository.save(produto);
+      return res.status(200).json(produto);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
 }
