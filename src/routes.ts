984e270727639744605293af5b4c71a0987ab24d
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-11-08 20:51:41 -0300

    alter routes

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,27 @@ import { VendaController } from "./controllers/VendaController";
 
 const routes = Router();
 
+//Rotas Tipo Produto
 routes.post("/tipos_produto", new TipoProdutoContoller().create);
+routes.get("/tipos_produto", new TipoProdutoContoller().listar);
+routes.get("/tipos_produto/:id", new TipoProdutoContoller().listarPorId);
+routes.delete("/tipos_produto/:id", new TipoProdutoContoller().delete);
+routes.put("/tipos_produto/:id", new TipoProdutoContoller().update);
+
+//Rotas Produtos
 routes.post("/produto", new ProdutoController().create);
+routes.get("/produto", new ProdutoController().listar);
+routes.get("/produto/:id", new ProdutoController().listarPorId);
+routes.delete("/produto/:id", new ProdutoController().delete);
+
+//Rotas Clientes
 routes.post("/cliente", new ClienteController().create);
+routes.get("/cliente", new ClienteController().listar);
+routes.get("/cliente/:id", new ClienteController().listarPorId);
+routes.delete("/cliente/:id", new ClienteController().delete);
+routes.put("/cliente/:id", new ClienteController().update);
+
+//Rotas Vendas
 routes.post("/venda", new VendaController().create);
 routes.get("/venda", new VendaController().listar);
 
