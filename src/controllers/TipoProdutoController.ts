93f1df24d9a645b4fce5ecf8e578c06508f4ff12
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-11-08 20:51:33 -0300

    Add CRUD TipoProduto

--- src/controllers/TipoProdutoController.ts
+++ src/controllers/TipoProdutoController.ts
@@ -22,4 +22,67 @@ export class TipoProdutoContoller {
       return res.status(500).json({ message: "Erro interno" });
     }
   }
+
+  async listar(req: Request, res: Response) {
+    try {
+      const tiposProduto = await TipoProdutoRepository.find();
+      return res.status(200).json(tiposProduto);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
+
+  async listarPorId(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const tipoProduto = await TipoProdutoRepository.findOne({
+        where: { id: parseInt(id) },
+      });
+      return res.status(200).json(tipoProduto);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const tipoProduto = await TipoProdutoRepository.findOne({
+        where: { id: parseInt(id) },
+      });
+      if (!tipoProduto) {
+        return res
+          .status(404)
+          .json({ message: "Tipo de produto não encontrado" });
+      }
+      await TipoProdutoRepository.remove(tipoProduto);
+      return res
+        .status(200)
+        .json({ message: "Tipo de produto removido com sucesso" });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { nome } = req.body;
+
+    try {
+      const tipoProduto = await TipoProdutoRepository.findOne({
+        where: { id: parseInt(id) },
+      });
+      if (!tipoProduto) {
+        return res
+          .status(404)
+          .json({ message: "Tipo de produto não encontrado" });
+      }
+      tipoProduto.nome = nome;
+      await TipoProdutoRepository.save(tipoProduto);
+      return res.status(200).json(tipoProduto);
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  }
 }
